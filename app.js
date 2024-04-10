5f3ebe516addb46a21b171a0523a5fda3da1923c
HarishG03 <131017755+HarishG03@users.noreply.github.com>
2024-04-11 00:27:52 +0530

    3 specified testcases failed after getting expected response

--- app.js
+++ app.js
@@ -28,9 +28,7 @@ app.post('/register/', async (request, response) => {
   const {username, password, name, gender} = request.body
   const query1 = `SELECT * FROM user 
   WHERE username = '${username}';`
-  console.log(query1)
   const result = await db.get(query1)
-  console.log(result)
   if (result !== undefined) {
     response.send('User already exists')
     response.status(400)
@@ -63,6 +61,7 @@ app.post('/login/', async (request, response) => {
       response.send('Invalid password')
     } else {
       const payload = {username: username}
+      console.log(payload)
       const token = jwt.sign(payload, 'Token_JWT')
       response.send({
         jwtToken: token,
@@ -87,7 +86,6 @@ const verifyToken = (request, response, next) => {
         response.send('Invalid JWT Token')
       } else {
         request.username = payload.username
-
         next()
       }
     })
@@ -107,10 +105,8 @@ app.get('/user/tweets/feed/', verifyToken, async (request, response) => {
   followingIds_req.forEach(obj => {
     arr.push(obj.followingIdreq)
   })
-  console.log(arr.value)
-  const query3 = `SELECT tweet ,date_time AS dateTime FROM Tweet
-  WHERE user_id in (${arr})
-  ORDER BY dateTime DESC ;`
+  console.log(arr)
+  const query3 = `SELECT username,tweet,date_time as dateTime FROM user INNER JOIN tweet ON user.user_id = tweet.user_id WHERE user.user_id IN (${arr}) ORDER BY date_time DESC LIMIT 4;`
   console.log(query3)
   const tweetRes = await db.all(query3)
   console.log(tweetRes)
@@ -118,13 +114,130 @@ app.get('/user/tweets/feed/', verifyToken, async (request, response) => {
   response.send(tweetRes)
 })
 
-app.delete('/delete/:id', async (request, response) => {
-  const {id} = request.params
+app.get('/user/following/', verifyToken, async (request, response) => {
+  const username = request.username
+  let arr = []
+  const query1 = `SELECT user_id FROM user
+  WHERE username = '${username}';`
+  const id_req1 = await db.get(query1)
+  const required_user_id = id_req1.user_id
+  const query2 = `SELECT following_user_id AS followingIdreq FROM follower
+  WHERE follower_user_id =  ${required_user_id};`
+  const followingIds_req = await db.all(query2)
+  followingIds_req.forEach(obj => {
+    arr.push(obj.followingIdreq)
+  })
+  console.log(arr)
+  const query3 = `Select name FROM user WHERE user.user_id IN (${arr});`
+  const result = await db.all(query3)
+  response.send(result)
+})
+
+app.get('/user/followers/', verifyToken, async (request, response) => {
+  const username = request.username
+  let arr = []
+  const query1 = `SELECT user_id FROM user
+  WHERE username = '${username}';`
+  const id_req1 = await db.get(query1)
+  const required_user_id = id_req1.user_id
+  const query2 = `SELECT follower_user_id AS followerIdreq FROM follower
+  WHERE following_user_id =  ${required_user_id};`
+  const followingIds_req = await db.all(query2)
+  followingIds_req.forEach(obj => {
+    arr.push(obj.followerIdreq)
+  })
+  console.log(arr)
+  const query3 = `Select name FROM user WHERE user.user_id IN (${arr});`
+  const result = await db.all(query3)
+  response.send(result)
+})
+
+app.get('/tweets/:tweetId/', verifyToken, async (request, response) => {
+  const {tweetId} = request.params
+  const username = request.username
+  const query1 = `SELECT tweet FROM tweet
+  WHERE tweet_id = ${tweetId};`
+  const result = await db.get(query1)
+  response.send(result)
+})
+app.get('/tweets/:tweetId/likes/', verifyToken, async (request, response) => {
+  const username = request.username
+  let arr = []
+  const query1 = `SELECT user_id FROM user
+  WHERE username = '${username}';`
+  const id_req1 = await db.get(query1)
+  const required_user_id = id_req1.user_id
+  const query2 = `SELECT following_user_id AS followingIdreq FROM follower
+  WHERE follower_user_id =  ${required_user_id};`
+  const followingIds_req = await db.all(query2)
+  followingIds_req.forEach(obj => {
+    arr.push(obj.followingIdreq)
+  })
+  console.log(arr)
+  const query3 = `Select user.name FROM USER INNER JOIN like ON user.userId = like.user_id WHERE user.user_id IN (${arr});`
+  const result = await db.all(query3)
+  response.send(result)
+})
+app.get('/tweets/:tweetId/replies/', verifyToken, async (request, response) => {
+  const username = request.username
+  let arr = []
+  const query1 = `SELECT user_id FROM user
+  WHERE username = '${username}';`
+  const id_req1 = await db.get(query1)
+  const required_user_id = id_req1.user_id
+  const query2 = `SELECT following_user_id AS followingIdreq FROM follower
+  WHERE follower_user_id =  ${required_user_id};`
+  const followingIds_req = await db.all(query2)
+  followingIds_req.forEach(obj => {
+    arr.push(obj.followingIdreq)
+  })
+  console.log(arr)
+  const query3 = `Select user.name FROM USER INNER JOIN reply ON user.userId = reply.user_id WHERE user.user_id IN (${arr});`
+  const result = await db.all(query3)
+  response.send(result)
+})
+app.get('/user/tweets/', verifyToken, async (request, response) => {
+  const username = request.username
+  const query1 = `SELECT user_id FROM user WHERE username = '${username}';`
+  const userId = await db.get(query1)
+  const reqUserId = userId.user_id
+  const query2 = `
+  SELECT tweet FROM tweet WHERE tweet.user_id = ${reqUserId};`
+  const result2 = await db.all(query2)
+  response.send(result2)
+})
+
+app.post('/user/tweets/', verifyToken, async (request, response) => {
+  const username = request.username
+  const {tweet} = request.body
+  const query1 = `SELECT user_id FROM user WHERE username = '${username}';`
+  const userId = await db.get(query1)
+  const dateTime = new Date()
+  console.log(userId)
+  const reqUserId = userId.user_id
+  const query2 = `
+  INSERT INTO tweet(tweet)
+  VALUES('${tweet}');`
+  await db.run(query2)
+  response.send('Created a Tweet')
+})
+
+app.delete('/tweets/:tweetId/', verifyToken, async (request, response) => {
+  const {tweetId} = request.params
   const query1 = `
-  DELETE FROM user 
-  WHERE user_id = ${id}`
-  await db.run(query1)
-  response.send('Deleted')
+  SELECT * FROM user INNER JOIN tweet ON user.user_id = tweet.user_id
+  WHERE tweet.tweet_id = ${tweetId};`
+  const result = await db.get(query1)
+  if (result === undefined) {
+    response.status(401)
+    response.send('Invalid Request')
+  } else {
+    const query2 = `
+    DELETE FROM tweet 
+  WHERE tweet.tweet_id = ${tweetId};`
+    await db.run(query2)
+    response.send('Tweet Removed')
+  }
 })
 
 module.exports = app
